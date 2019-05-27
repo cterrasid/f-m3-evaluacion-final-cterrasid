13dd8d1d8ccbe64a800e466cadecfd3e432d72bd
cterrasid <clarette.terrasi@gmail.com>
2019-05-27 19:24:30 +0200

    Run build

---
+++ docs/precache-manifest.6fea59da7a5519a6d1698d6de31a557d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89fcac54b06002f2840ca568fcdc537",
+    "url": "./index.html"
+  },
+  {
+    "revision": "185a35d9488e96d52f34",
+    "url": "./static/css/main.f1f9ac88.chunk.css"
+  },
+  {
+    "revision": "47ed7bbb11c498ce96a1",
+    "url": "./static/js/2.d523a628.chunk.js"
+  },
+  {
+    "revision": "185a35d9488e96d52f34",
+    "url": "./static/js/main.e156d2b5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ccab2c1d952caa54cd452b88becceac3",
+    "url": "./static/media/Gryffindor.ccab2c1d.png"
+  },
+  {
+    "revision": "12d81696b740c7895018937d6af9ea73",
+    "url": "./static/media/Hufflepuff.12d81696.png"
+  },
+  {
+    "revision": "3ab238096585380e82fed3a32db035ab",
+    "url": "./static/media/Ravenclaw.3ab23809.png"
+  },
+  {
+    "revision": "9791ef71a1c6ffb6a44ff94f15a01fbb",
+    "url": "./static/media/Slytherin.9791ef71.png"
+  }
+]);
